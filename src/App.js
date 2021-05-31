29e10c2c3a34c94c53529f0ec65e60bb4a1a0867
unknown <rahulkeswani030@gmail.com>
2021-05-31 12:00:04 +0530

    Added all necessary files

---
+++ src/App.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import './App.css';
+import axios from 'axios';
+import React, {useState,useEffect} from 'react';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+function App() {
+  const [posts,setPosts]=useState([]);
+  const [loading, setLoading]=useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [currentPage, setCurrentPage]=useState(1);
+  const [postsPerPage,setPostsPerPage]=useState(10);
+  useEffect(()=>{
+    const fetchPosts=async () =>{
+      setLoading(true);
+      const res=await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  },[]);
+  const paginate=(pageNumber) => setCurrentPage(pageNumber);
+  // console.log(posts);
+  // get current post
+  const indexOfLastPost=currentPage*postsPerPage;
+  const indexOfFirstPost=indexOfLastPost-postsPerPage;
+  const currentPosts=posts.slice(indexOfFirstPost,indexOfLastPost);
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">Simple Pagination Example</h1>
+     <Posts posts={currentPosts} loading={loading}/>
+     <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pagination.js
@@ -0,0 +1,24 @@
+/* eslint-disable array-callback-return */
+import React from 'react'
+
+function Pagination({postsPerPage,totalPosts,paginate}) {
+    const pageNumbers=[];
+    for(let i=1;i<=Math.ceil(totalPosts/postsPerPage);i++)
+    {
+        pageNumbers.push(i);
+    }
+    return (
+        <nav>
+            <ul className="pagination">
+            {pageNumbers.map(number =>(
+
+                <li key={number} className='page-item'>
+                <a href='#xyz' className='page-link' onClick={() => paginate(number)}>{number}</a>
+            </li>
+            ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination
